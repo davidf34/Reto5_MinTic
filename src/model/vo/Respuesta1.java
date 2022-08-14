f8910413787dae1ba459ced8e6cd4eee9327e9ae
David Sandoval <david.sandoval-g@mail.escuelaing.edu.co>
2022-08-13 22:06:10 -0500

    Ultimas modificaciones

--- src/model/vo/Respuesta1.java
+++ src/model/vo/Respuesta1.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model.vo;
 
-/**
- *
- * @author DIDIER GUERRERO
- */
+
 public class Respuesta1 {
     private Integer id_lider;
     private String nombre;
--- src/model/vo/Respuesta2.java
+++ src/model/vo/Respuesta2.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model.vo;
 
-/**
- *
- * @author DIDIER GUERRERO
- */
 public class Respuesta2 {
     private Integer id_proyecto;
     private String constructora;
